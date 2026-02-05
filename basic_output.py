caff725a51ff2f5f66284311ad128f79bab361de
Akumath-Saniya <akumathsaniya@gmail.com>
2026-02-05 18:29:19 +0530

    adding first task

---
+++ basic_output.py
@@ -0,0 +1,11 @@
+print("name = Akumath Saniya")
+print("age = 20")
+print("The programming language i like = python")
+print("My current goal is to learn python to apply in real world")
+numbers=[60,80,100,700]
+for value in numbers:
+     print(value)
+num1=int(input("enter number 1 "))
+num2=int(input("enter number 2 "))
+print()
+print(num1+num2)
