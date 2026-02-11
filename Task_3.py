a2e32dc62144ded11e7d7d9389fbfdf51016433f
Akumath-Saniya <akumathsaniya@gmail.com>
2026-02-11 21:35:56 +0530

    Adding task 3

---
+++ Task_3.py
@@ -0,0 +1,26 @@
+#storing name of student 
+student_name="Saniya"
+print("Student name : ",student_name)
+print(type(student_name))
+#Storing age of the student
+age=20
+print("Student age : ",age)
+print(type(age))
+#storing marks os student 
+math_marks=78.32
+phy_marks=89.5
+chem_marks=82.65
+print("Student marks in: maths: ",math_marks,"physics: ",phy_marks,"Chemistry: ",chem_marks)
+print(type(math_marks))
+#storing a complex value
+complex_value=3+5j
+print("A complex result: ",complex_value)
+print(type(complex_value))
+#storing subjects
+subjects=["Maths","Physics","Chemistry"]
+print("Student subjects are ",subjects)
+print(type(subjects))
+#Storing fixed date of birth of student
+dob=(5,4,2005)
+print("Date of birth of student ",dob)
+print(type(dob))
